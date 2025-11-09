0c84993000e1ee2ba3075f347ad6478f86de763b
Sanaa Thompson <thompsonsanaa48@gmail.com>
2025-11-09 02:17:46 -0500

    Added Javascript functionality for newsletter subscription form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+
+window.onload = function () {
+
+	const form = document.querySelector('.newsletter form');
+	const emailInput = document.querySelector('#email');
+	const messageDiv = document.querySelector('.message');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+
+		const email = emailInput.value.trim();
+
+		if(email === "") {
+		
+			messageDiv.textContent = "Please enter a valid email address.";
+			messageDiv.style.color = "red";
+		} else {
+		
+			messageDiv.textContent = "Thankyou! Your email address " + email + " has been added to our mailing list!";
+			messageDiv.style.color = "white";
+		}
+
+		emailInput.value = "";
+	});
+};
+	
